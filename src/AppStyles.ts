81defec5790ac8324f75cb666177e7b02aad53cf
elizabeth lands <elizabeth_lands@hotmail.com>
2023-02-06 10:05:15 +0000

    feat: basic app set-up

---
+++ src/AppStyles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 80%;
+  max-width: 700px;
+  margin: 0 auto;
+  background: green;
+  color: white;
+  padding: 40px;
+`;
+
+export {
+  Container
+}
